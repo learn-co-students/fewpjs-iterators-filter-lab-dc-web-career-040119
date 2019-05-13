ddaea854cde7564552a146de3707f6ff3a487f26
errolgh <ewatsonrei@gmail.com>
2019-05-13 07:54:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+// let even = arr.filter(n => {
+//   return n % 2 === 0;
+// });
+// let findMatching = a
+
+let fuzzyMatch = drivers.filter(n => {
+  return n === "Sa";
+});
